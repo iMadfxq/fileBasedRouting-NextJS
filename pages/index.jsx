a3999570220453bfce6b01cbeba314e48f37008e
iMadfxq <imadfxq@gmail.com>
2023-01-09 20:07:28 -0500

    Writing code that doesnt work, but that helps me learn and remember

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const Home = () => {
+const Home = ({objert}) => {
   const router = useRouter()
   return (
     <>
       <h1>Home</h1>
+      <h4>{objert}</h4>
       <Link href={'portfolio'}>Portfolio</Link>
       <Link href={'/portfolio/OscarGomez'}>Test</Link>
       <button onClick={() => {
@@ -16,3 +17,16 @@ const Home = () => {
 };
 
 export default Home;
+
+// useAWR
+
+export async function getStaticProps() {
+  return {
+    props: {
+      posts,
+      users,
+      whatever,
+      objert: 'carro'
+    }
+  }
+}
